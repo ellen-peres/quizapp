92ca852f2f594c6e9024b72bcc662c621f5ae552
Ellen Peres <ellenperessantos@gmail.com>
2024-06-06 22:22:58 +0000

    Criação de um helper responsável por mudar o tema

--- script.js
+++ script.js
@@ -1,38 +1,15 @@
-let tema = "claro"
+import { trocarTema , verificarTema } from "./helpers/tema-helper.js"
+
+
 
 const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
-
 const body = document.querySelector("body")
+botaoTema.addEventListener("click", () =>  {
+    trocarTema( body, botaoTema)
+})
 
-function trocarTema() {
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
+verificarTema( body, botaoTema)
 
-   
 
-    if (tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-        botaoTema
-    } else {
-         body.classList.remove("escuro");
-         localStorage.setItem("tema", "claro")
-    }
 
-}
-
-function verificarTema() {
-    if(localStorage.getItem("tema")){
-        tema = localStorage.getItem("tema")
-    }
-
-    if ( tema === "escuro"){
-        body.classList.add("escuro")
-
-    }
-}
-
-verificarTema()
     
