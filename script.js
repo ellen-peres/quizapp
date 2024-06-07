7537bb881ffceb62cbd08ed4c3f773626b910eb2
Ellen Peres <ellenperessantos@gmail.com>
2024-06-07 01:41:38 +0000

    Adicionada a função para escolher o tema do quiz no index.html

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ botaoTema.addEventListener("click", () =>  {
 
 verificarTema( body, botaoTema)
 
+const botoesAssunto = document.querySelectorAll(".assuntos button")
 
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento) {
+    const assunto = evento.target.innerTest
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
 
     
